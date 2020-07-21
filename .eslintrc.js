2db7ffaa9327823cd95bd7a22218aef89cd908f8
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-07-21 14:10:02 +0800

    configure prettier and linter

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "env": {
+        "es2020": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "prettier"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};

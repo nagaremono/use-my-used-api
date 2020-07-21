168fcc17f64c908f05bf9ebf2914bc786a0d786c
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-07-21 15:16:44 +0800

    created basic express app

---
+++ app.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import createError from 'http-errors';
+import logger from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json();
+});
+
+app.listen(3000, () => {
+  console.log('Now listening on port 3000');
+});

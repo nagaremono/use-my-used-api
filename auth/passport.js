ac6def8da13d91f7098cda65161aed3588f47fdf
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-07-22 22:09:45 +0800

    defined strategy for user login

---
+++ auth/passport.js
@@ -0,0 +1,26 @@
+import passport from 'passport';
+import passportLocal from 'passport-local';
+import bcrypt from 'bcrypt';
+import User from '../models/User.js';
+
+const LocalStrategy = passportLocal.Strategy;
+
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username' });
+      }
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: 'Incorrect password' });
+        }
+      });
+    });
+  })
+);

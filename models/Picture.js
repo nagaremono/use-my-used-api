e5f1032aef81cf030270acf6755574637eee572a
guruhedipurwanto <guruhedipurwanto@gmail.com>
2020-07-23 20:10:33 +0800

    redefined image to be base64 string

--- models/Picture.js
+++ models/Picture.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const PictureSchema = new Schema({
-  image: { data: Buffer, contentType: String },
+  image: { type: String },
   user: { type: Schema.Types.ObjectId, ref: 'User' },
 });
 
